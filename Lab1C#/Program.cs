449dd999b5c572dd14d5b752d6746b86b50087e0
VNatlia <nvolynets371@gmail.com>
2025-02-26 22:50:49 +0200

    Добавьте файлы проекта.

---
+++ Lab1C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Lab2C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
