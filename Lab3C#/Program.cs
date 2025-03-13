4340145f0b22e056c124a971ce3d53cf20c00503
VNatlia <nvolynets371@gmail.com>
2025-03-14 01:00:59 +0200

    Add files via upload

--- Lab3C#/Program.cs
+++ Lab3C#/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Lab3C_
+﻿namespace StringApp
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введіть рядок для S2: ");
+            string input2 = Console.ReadLine();
+
+            Console.Write("Введіть рядок для S3: ");
+            string input3 = Console.ReadLine();
+
+            StringClass S2 = new StringClass(val: input2); //val: - іменований аргумент 
+            StringClass S3 = new StringClass(input3);
+
+
+            // Вирахування символу # (перевантаж оператор -)
+            S2 = S2 - '#';
+            // Додавання S2 + S3 (перевантаж. оператор +)
+            StringClass S1 = S2 + S3;
+
+            Console.WriteLine("\nРезультати:");
+            Console.WriteLine("S1 (S2 + S3): " + S1.GetValue());
+            Console.WriteLine("Довжина S1: " + S1.GetLength());
         }
     }
 }
---
+++ Lab3C#/StringClass.cs
@@ -0,0 +1,47 @@
+﻿namespace StringApp
+{
+    public class StringClass
+    {
+        private string value;
+
+        public StringClass() //порожн. конструктор
+        {
+            value = "";
+        }
+
+        public StringClass(string val) //конструкт. з параметром
+        {
+            value = val;
+        }
+
+        public StringClass(StringClass other) //конструкт. копіювання
+        {
+            value = other.value;
+        }
+        // Метод з аргументами за замовчуванням
+        public void PrintString(string prefix = "Value: ")
+        {
+            Console.WriteLine(prefix + value);
+        }
+
+        public static StringClass operator +(StringClass s1, StringClass s2) //перевантаж. оператора+
+        {
+            return new StringClass(s1.value + s2.value);
+        }
+
+        public static StringClass operator -(StringClass s, char ch) //перевантаж. оператора -
+        {
+            return new StringClass(s.value.Replace(ch.ToString(), ""));
+        }
+    
+        public int GetLength()
+        {
+            return value.Length;
+        }
+
+        public string GetValue()
+        {
+            return value;
+        }
+    }
+}
