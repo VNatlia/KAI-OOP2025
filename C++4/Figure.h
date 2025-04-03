05e0a54222fb86bb0cf17687b0d5ca5abd971897
VNatlia <nvolynets371@gmail.com>
2025-04-03 23:51:45 +0300

    Update Figure.h

--- C++4/Figure.h
+++ C++4/Figure.h
@@ -6,16 +6,15 @@ protected:
     double x1, y1, x2, y2, x3, y3;
 
 public:
-    // Конструктори
     Figure();
     Figure(double, double, double, double, double, double);
     Figure(const Figure& other);
   
 
-    // Деструктор
+ 
     virtual ~Figure();
 
-    // Метод для обчислення довжини сторони
+   
     double sideLength(double, double, double, double) const;
 };
 
