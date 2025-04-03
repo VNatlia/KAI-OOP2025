86af63bdabd4b30ce989120e28cd7a4f5882c905
VNatlia <nvolynets371@gmail.com>
2025-04-03 23:52:17 +0300

    Update Triangle.cpp

--- C++4/Triangle.cpp
+++ C++4/Triangle.cpp
@@ -1,27 +1,26 @@
 #include "Triangle.h"
 #include <cmath>
 
-// Конструктор за замовчуванням
+
 Triangle::Triangle() : Figure() {}
 
-// Конструктор з параметрами (явний виклик конструктора базового класу)
 Triangle::Triangle(double ax1, double ay1, double ax2, double ay2, double ax3, double ay3)
     : Figure(ax1, ay1, ax2, ay2, ax3, ay3) {}
 
-// Конструктор копіювання
+
 Triangle::Triangle(const Triangle& other) : Figure(other) {}
 
-// Деструктор
+
 Triangle::~Triangle() {}
 
-// Обчислення периметра
+
 double Triangle::perimeter() const {
     return sideLength(x1, y1, x2, y2) +
         sideLength(x2, y2, x3, y3) +
         sideLength(x3, y3, x1, y1);
 }
 
-// Обчислення площі
+
 double Triangle::area() const {
     double a = sideLength(x1, y1, x2, y2);
     double b = sideLength(x2, y2, x3, y3);
