f773e60ddd284986fb6a78bf5adf0d84030449f5
VNatlia <nvolynets371@gmail.com>
2025-04-03 23:52:37 +0300

    Update Triangle.h

--- C++4/Triangle.h
+++ C++4/Triangle.h
@@ -5,16 +5,12 @@
 
 class Triangle : public Figure {
 public:
-    // Конструктори
     Triangle();
     Triangle(double, double, double, double, double, double);
     Triangle(const Triangle& other);
   
-
-    // Деструктор
     ~Triangle();
 
-    // Методи
     double perimeter() const;
     double area() const;
 };
