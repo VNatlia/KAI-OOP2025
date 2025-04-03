b23c813bb26090c9fa57e24a286f5b46fc9d4dad
VNatlia <nvolynets371@gmail.com>
2025-04-03 23:53:12 +0300

    Update main.cpp

--- C++4/main.cpp
+++ C++4/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     Triangle t(0, 0, 3, 0, 3, 4);
-    std::cout << "Периметр: " << t.perimeter() << "\n";
-    std::cout << "Площа: " << t.area() << "\n";
+    std::cout << "permeter: " << t.perimeter() << "\n";
+    std::cout << "area: " << t.area() << "\n";
     return 0;
 }
